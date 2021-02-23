a2c630e4da3e660fb1a29a0d5c5c4e7a8c7abcd7
n-flam <nadja.flam@gmx.net>
2021-02-23 15:37:09 +0800

    Create config, connection.js and orm.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Chicken Bocata');
+INSERT INTO burgers (name, devoured) VALUES ('Lamb Bocata', true);
+INSERT INTO burgers (name, devoured) VALUES ('Beef Bocata', true);
+INSERT INTO burgers (name, devoured) VALUES ('Avocado Bocata', true);
+INSERT INTO burgers (name) VALUES ('Eggplant Bocata')
