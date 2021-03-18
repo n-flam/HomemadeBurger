36e94893330bda0ffa823241b35d6c7f6f09624f
n-flam <nadja.flam@gmx.net>
2021-03-18 19:10:07 +0800

    connection config

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,17 @@ let connection = mysql.createConnection({
   database: 'burger_db',
 });
 
-
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "keehlaep2o3g5f8c",
+    password: "wf4zid8juhe57387",
+    database: "rlb0l6ktj5kfd62r",
+  });
+}
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
